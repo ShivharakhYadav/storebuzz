db98eca0cafbe5daab0b9de5af2ca420ce5f9d31
Shivharakh Yadav <yadavshivharakhyadav@gmail.com>
2022-03-17 07:57:39 +0530

    Update CardsItems.js

--- src/Components/Cards/CardsItems.js
+++ src/Components/Cards/CardsItems.js
@@ -7,6 +7,8 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
+import { purple } from '@mui/material/colors';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 // import { makeStyles } from '@mui/styles';
 import { Container } from '@mui/material'
 // const useStyles = makeStyles((theme)=>({
@@ -16,6 +18,50 @@ import { Container } from '@mui/material'
 //         backgroundColor:theme.palette.red[200]
 //     }
 // }))
+import { styled } from '@mui/material/styles';
+const BootstrapButton = styled(Button)({
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: '#0063cc',
+    borderColor: '#0063cc',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#0062cc',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#0062cc',
+      borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  });
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  }));
 function CardsItems() {
     // const classes = useStyles();
     return (
@@ -43,7 +89,7 @@ function CardsItems() {
                                     </CardContent>
                                 </CardActionArea>
                                 <CardActions>
-                                    <Button size="small">Share</Button>
+                                <ColorButton><Button size="small" color="primary"><ShoppingCartOutlinedIcon/>Add To Cart</Button></ColorButton>
                                     <Button size="small">Learn More</Button>
                                 </CardActions>
                             </Card>    
